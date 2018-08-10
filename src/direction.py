d00c59c5a6c4d32a539613b00ff10224a4a7cb54
vickiiiiichen <36083638+vickiiiiichen@users.noreply.github.com>
2018-08-10 18:30:11 -0400

    Create direction.py

---
+++ src/direction.py
@@ -0,0 +1,32 @@
+import pyshark
+import sys
+
+cap = pyshark.FileCapture(sys.argv[1])
+protocol = []
+size = []
+summary = []
+
+for pkt in cap:
+    try:
+        for [src,dst] in summary:
+            if src == pkt.ip.src and dst == pkt.ip.dst:
+                index = summary.index([src,dst])
+                size[index] = size[index] + int(pkt.length)
+                break
+        else:
+            pair = [pkt.ip.src, pkt.ip.dst]
+            summary.append(pair)
+            size.append(int(pkt.length))
+            protocol.append(pkt.highest_layer)
+
+    except AttributeError as e:
+            pass
+
+
+print('-----------------------------------------------------------')
+print('| {:^20s} | {:^20s} |'.format("Direction", "Size"))
+print('-----------------------------------------------------------')
+
+for i in range(0, len(summary)):
+    print('| {:^10s} | {:^10s}|'.format(str(summary[i]), str(size[i])))
+    print('-----------------------------------------------------------')
