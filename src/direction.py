1ea9efa21c0d098bb44dc4cb2a9a48daadcf6110
vickiiiiichen <36083638+vickiiiiichen@users.noreply.github.com>
2018-08-16 17:30:34 -0400

    Update direction.py

--- src/direction.py
+++ src/direction.py
@@ -1,68 +1,51 @@
 import pyshark
 import sys
+import ipaddress
 
 cap = pyshark.FileCapture(sys.argv[1])
+protocol = []
 size = []
+number = []
 summary = []
-
-
-def partition(arr_to_sort, arr1, low, high):
-    i = (low - 1)  # index of smaller element
-    pivot = arr_to_sort[high]  # pivot
-
-    for j in range(low, high):
-
-        # If current element is larger than or
-        # equal to pivot
-        if arr_to_sort[j] >= pivot:
-            # increment index of larger element
-            i = i + 1
-            arr_to_sort[i], arr_to_sort[j] = arr_to_sort[j], arr_to_sort[i]
-            arr1[i], arr1[j] = arr1[j], arr1[i]
-
-    arr_to_sort[i + 1], arr_to_sort[high] = arr_to_sort[high], arr_to_sort[i + 1]
-    arr1[i + 1], arr1[high] = arr1[high], arr1[i + 1]
-
-    return i + 1
-
-
-# Function to do Quick sort
-def quick_sort(arr_to_sort, arr1, low, high):
-    if low < high:
-        # pi is partitioning index, arr_to_sort[p] is now
-        # at right place
-        pi = partition(arr_to_sort, arr1, low, high)
-
-        # Separately sort elements before
-        # partition and after partition
-        quick_sort(arr_to_sort, arr1, low, pi - 1)
-        quick_sort(arr_to_sort, arr1, pi + 1, high)
-
-
-def format_print():
-    print('-----------------------------------------------------------')
-    print('| {:^20s} | {:^20s} |'.format("Direction", "Size"))
-    print('-----------------------------------------------------------')
-
-    for i in range(0, len(summary)):
-        print('| {:^10s} | {:^10s}|'.format(str(summary[i]), str(size[i])))
-        print('-----------------------------------------------------------')
-
 
 for pkt in cap:
     try:
-        for [src, dst] in summary:
+        for [src,dst] in summary:
             if src == pkt.ip.src and dst == pkt.ip.dst:
-                index = summary.index([src, dst])
+                index = summary.index([src,dst])
                 size[index] = size[index] + int(pkt.length)
+                number[index] = number[index] + 1
                 break
         else:
             pair = [pkt.ip.src, pkt.ip.dst]
             summary.append(pair)
             size.append(int(pkt.length))
+            number.append(1)
+            protocol.append(pkt.highest_layer)
 
     except AttributeError as e:
             pass
 
-quick_sort(size, summary, 0, len(size)-1)
-format_print()
+
+print('-----------------------------------------------------------')
+print('| {:^20s} | {:^20s} |{:^20s}'.format("Direction", "Size","Number"))
+print('-----------------------------------------------------------')
+local = 0
+multicast = 0
+cloud = 0
+totaln = 0
+for i in range(0, len(summary)):
+	totaln = totaln + number[i]
+	if ipaddress.ip_address(summary[i][0]).is_private and ipaddress.ip_address(summary[i][1]).is_private :
+			local = local+number[i] 
+	elif ipaddress.ip_address(summary[i][0]).is_multicast or ipaddress.ip_address(summary[i][1]).is_multicast:
+			multicast = multicast +number[i]
+	else:
+		cloud = cloud +number[i]
+
+for i in range(0, len(summary)):
+    print('| {:^10s} | {:^10s}|{:^10s} |'.format(str(summary[i]), str(size[i]),str(number[i])))
+    print('-----------------------------------------------------------')
+print("Local:",local/totaln)
+print("Multicast:",multicast/totaln)
+print("Cloud:",cloud/totaln)
