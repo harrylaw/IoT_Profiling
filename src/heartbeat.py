b565fe51374dd373bb85b7b5ecd1b4b66ee08f7c
harrylaw <harrylaw86@gmail.com>
2018-08-21 19:05:38 -0400

    Created heartbeat.py

---
+++ src/heartbeat.py
@@ -0,0 +1,21 @@
+import pyshark
+import sys
+from filter import create_list, print_list, ask_for_device, filter_packets
+
+
+def calculate_heartbeat(cap_sum):  # use cap_sum
+    time_difference = []
+    for i in range(1, len(cap_sum)):
+        time_difference.append(float(cap_sum[i].time) - float(cap_sum[i-1].time))
+    heartbeat = sum(time_difference) / (len(cap_sum) - 1)
+    return heartbeat
+
+
+if __name__ == "__main__":
+    unfiltered_cap = pyshark.FileCapture(sys.argv[1])
+    unfiltered_cap_sum = pyshark.FileCapture(sys.argv[1], only_summaries=True)
+    create_list(unfiltered_cap)
+    print_list()
+    device_number = ask_for_device()
+    cap, cap_sum = filter_packets(device_number, unfiltered_cap, unfiltered_cap_sum)
+    print("heartbeat = {:.4f}".format(calculate_heartbeat(cap_sum)))
